efe327a577de878cab70d085553d3d37edb1696f
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-07-28 01:32:49 +0300

    User service improvements
    Simplified communication with back end, fixed city display error
    
    Also added 'npm run start-local' to launch with proxy

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { User } from '../domain/user';
 import { BackendRequest } from '../domain/backend-request';
@@ -17,20 +16,6 @@ export class UserService {
   }
 
   fetchUsers(request: BackendRequest): Observable<User[]> {
-    const asyncOptions = {
-      observe: 'events' as 'body',
-      responseType: 'text' as 'json',
-      reportProgress: true,
-      withCredentials: true
-    };
-    return this.http.post<any>(URL, request, asyncOptions)
-      .pipe(
-        filter(e => e.type === 3 && e.partialText),
-        map(e => {
-          return e.partialText.trim()
-            .split('\n')
-            .map(userDtoJson => JSON.parse(userDtoJson) as User);
-        })
-      );
+    return this.http.post<User[]>(URL, request);
   }
 }
